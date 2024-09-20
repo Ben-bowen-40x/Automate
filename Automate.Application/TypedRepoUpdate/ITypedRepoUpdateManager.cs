4d6d578da8dfacdc85694b9666e23f67030dc3e3
WeCodeAwayYourProblems <90477602+WeCodeAwayYourProblems@users.noreply.github.com>
2024-09-20 14:01:08 -0600

    Create ITypedRepoUpdateManager.cs

---
+++ Automate.Application/TypedRepoUpdate/ITypedRepoUpdateManager.cs
@@ -0,0 +1,9 @@
+﻿using Automate.Application.InfrastructureInterfaces;
+using CSharpFunctionalExtensions;
+
+namespace Automate.Application.TypedRepoUpdate;
+
+public interface ITypedRepoUpdateManager
+{
+    Result Manage<TEntity, TTarget>(DwhQueryType type, DwhConnectionType connection, string repoJson, bool hardUpdate) where TEntity : class, IPhoneNumberCompatible;
+}
