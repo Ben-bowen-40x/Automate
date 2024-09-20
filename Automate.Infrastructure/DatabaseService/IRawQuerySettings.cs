3e6db4b5dcd77806171b80c6d70dd9f5c0d2c200
WeCodeAwayYourProblems <90477602+WeCodeAwayYourProblems@users.noreply.github.com>
2024-09-20 14:01:35 -0600

    Create IRawQuerySettings.cs

---
+++ Automate.Infrastructure/DatabaseService/IRawQuerySettings.cs
@@ -0,0 +1,31 @@
+﻿namespace Automate.Infrastructure.DatabaseService;
+
+public interface IRawQuerySettings
+{
+    string? QueryDateFormat { get; set; }
+
+    // Basics
+    string? CallBasicNumerical { get; set; }
+    string? CustomerBasicNumerical { get; set; }
+    string? CallBasic { get; set; }
+    string? CallBasicAddon { get; set; }
+    string? CustomerBasic { get; set; }
+
+    // Message
+    string? MessageCallQuery1 { get; set; }
+    string? MessageCallQuery2 { get; set; }
+    string? MessageCallQuery3 { get; set; }
+    string? MessageCustQuery2 { get; set; }
+    string? MessageCustQuery3 { get; set; }
+
+    // Discrepancy
+    string? Discrepancy { get; set; }
+    string? Discrepancy2 { get; set; }
+    string? OriginalDiscrepancy { get; set; }
+
+    // Contacts
+    string? ContactUpdate1 { get; set; }
+    string? ContactUpdate2 { get; set; }
+    string? ContactUpdate3 { get; set; }
+    ulong ContactUpdateNumber { get; set; }
+}
