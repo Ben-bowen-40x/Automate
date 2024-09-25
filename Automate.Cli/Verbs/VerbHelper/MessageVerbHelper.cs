b1c3c83290530d10e3ebf956d40a0241ab348ddb
WeCodeAwayYourProblems <90477602+WeCodeAwayYourProblems@users.noreply.github.com>
2024-09-25 14:06:08 -0600

    Update MessageVerbHelper.cs
    Uses tuple-switch statement directly instead of a ternary and local functions

--- Automate.Cli/Verbs/VerbHelper/MessageVerbHelper.cs
+++ Automate.Cli/Verbs/VerbHelper/MessageVerbHelper.cs
@@ -21,7 +21,42 @@ public class MessageVerbHelper
     public const string HelpText = "Choose which type of message file you wish to analyze. The following options are case-sensitive: Pan, Leaf, LeafRepo, ManualWebForm, GAdsLeaf, GAdsLeafRepo";
     internal static Result<FileInfo> Execute(bool append, IServiceProvider service, string messageLocation, string callQueryLocation, string customerQueryLocation, string reportLocation, MessageCsvType messageType)
     {
-        return append
+        var generator = service.GetRequiredService<IMessageAnalysisManager>();
+        var appender = service.GetRequiredService<IMessageAnalysisReportManager>();
+        return (messageType, append) switch
+        {
+            // Pan
+            (MessageCsvType.Pan, true) => appender.Manage<SplitDateMountainOffsetMsgCol>(MessageCsvType.Pan.ToString(), messageLocation, callQueryLocation, customerQueryLocation, reportLocation),
+            (MessageCsvType.Pan, false) => generator.Manage<SplitDateMountainOffsetMsgCol>(MessageCsvType.Pan.ToString(), messageLocation, callQueryLocation, customerQueryLocation, reportLocation),
+
+            // GAdsLeaf
+            (MessageCsvType.GAdsLeaf, true) => appender
+                .Manage<UnifiedDateUnchangedOffset_SeparateGclid_SourceCantBeEmpty_MsgCol>(MessageCsvType.GAdsLeaf.ToString(), messageLocation, callQueryLocation, customerQueryLocation, reportLocation),
+            (MessageCsvType.GAdsLeaf, false) => generator
+                .Manage<UnifiedDateUnchangedOffset_SeparateGclid_SourceCantBeEmpty_MsgCol>(MessageCsvType.GAdsLeaf.ToString(), messageLocation, callQueryLocation, customerQueryLocation, reportLocation),
+
+            // GAdsLeafRepo
+            (MessageCsvType.GAdsLeafRepo, true) => appender.Manage<MessageClass>(MessageCsvType.GAdsLeafRepo.ToString(), messageLocation, callQueryLocation, customerQueryLocation, reportLocation),
+            (MessageCsvType.GAdsLeafRepo, false) => generator.Manage<MessageClass>(MessageCsvType.GAdsLeafRepo.ToString(), messageLocation, callQueryLocation, customerQueryLocation, reportLocation),
+
+            // ManualWebForm
+            (MessageCsvType.ManualWebForm, true) => appender.Manage<MessageClass>(MessageCsvType.GAdsLeafRepo.ToString(), messageLocation, callQueryLocation, customerQueryLocation, reportLocation),
+            (MessageCsvType.ManualWebForm, false) => generator.Manage<NoTimeMsgCol>(MessageCsvType.ManualWebForm.ToString(), messageLocation, callQueryLocation, customerQueryLocation, reportLocation),
+
+            // Leaf
+            (MessageCsvType.Leaf, true) => appender.Manage<UnifiedDateUnchangedOffset_SeparateGclid_MsgCol>(MessageCsvType.Leaf.ToString(), messageLocation, callQueryLocation, customerQueryLocation, reportLocation),
+            (MessageCsvType.Leaf, false) => generator.Manage<UnifiedDateUnchangedOffset_SeparateGclid_MsgCol>(MessageCsvType.Leaf.ToString(), messageLocation, callQueryLocation, customerQueryLocation, reportLocation),
+
+            // LeafRepo
+            (MessageCsvType.LeafRepo, true) => appender.Manage<MessageClass>(MessageCsvType.LeafRepo.ToString(), messageLocation, callQueryLocation, customerQueryLocation, reportLocation),
+            (MessageCsvType.LeafRepo, false) => generator.Manage<MessageClass>(MessageCsvType.LeafRepo.ToString(), messageLocation, callQueryLocation, customerQueryLocation, reportLocation),
+
+            // Default
+            _ => generator.Manage<UnifiedDateUnchangedOffset_SeparateGclid_MsgCol>(MessageCsvType.Leaf.ToString(), messageLocation, callQueryLocation, customerQueryLocation, reportLocation)
+        };
+        /* // Old Code -- This is here for verification purposes and debugging only
+         // TODO: delete old code here, once everything is verified
+         * return append
             ? Append(service, messageLocation, callQueryLocation, customerQueryLocation, reportLocation, messageType)
             : GenerateNew(service, messageLocation, callQueryLocation, customerQueryLocation, reportLocation, messageType);
 
@@ -31,13 +66,13 @@ public class MessageVerbHelper
             var m = service.GetRequiredService<IMessageAnalysisManager>();
             return messageType switch
             {
-                MessageCsvType.Pan => m.ManageMessageAnalysis<SplitDateMountainOffsetMsgCol>(MessageCsvType.Pan.ToString(), messageLocation, callQueryLocation, customerQueryLocation, reportLocation),
-                MessageCsvType.GAdsLeaf => m.ManageMessageAnalysis<UnifiedDateUnchangedOffset_SeparateGclid_SourceCantBeEmpty_MsgCol>(MessageCsvType.GAdsLeaf.ToString(), messageLocation, callQueryLocation, customerQueryLocation, reportLocation),
-                MessageCsvType.GAdsLeafRepo => m.ManageMessageAnalysis<MessageClass>(MessageCsvType.GAdsLeafRepo.ToString(), messageLocation, callQueryLocation, customerQueryLocation, reportLocation),
-                MessageCsvType.ManualWebForm => m.ManageMessageAnalysis<NoTimeMsgCol>(MessageCsvType.ManualWebForm.ToString(), messageLocation, callQueryLocation, customerQueryLocation, reportLocation),
-                MessageCsvType.Leaf => m.ManageMessageAnalysis<UnifiedDateUnchangedOffset_SeparateGclid_MsgCol>(MessageCsvType.Leaf.ToString(), messageLocation, callQueryLocation, customerQueryLocation, reportLocation),
-                MessageCsvType.LeafRepo => m.ManageMessageAnalysis<MessageClass>(MessageCsvType.LeafRepo.ToString(), messageLocation, callQueryLocation, customerQueryLocation, reportLocation),
-                _ => m.ManageMessageAnalysis<UnifiedDateUnchangedOffset_SeparateGclid_MsgCol>(MessageCsvType.Leaf.ToString(), messageLocation, callQueryLocation, customerQueryLocation, reportLocation),
+                MessageCsvType.Pan => m.Manage<SplitDateMountainOffsetMsgCol>(MessageCsvType.Pan.ToString(), messageLocation, callQueryLocation, customerQueryLocation, reportLocation),
+                MessageCsvType.GAdsLeaf => m.Manage<UnifiedDateUnchangedOffset_SeparateGclid_SourceCantBeEmpty_MsgCol>(MessageCsvType.GAdsLeaf.ToString(), messageLocation, callQueryLocation, customerQueryLocation, reportLocation),
+                MessageCsvType.GAdsLeafRepo => m.Manage<MessageClass>(MessageCsvType.GAdsLeafRepo.ToString(), messageLocation, callQueryLocation, customerQueryLocation, reportLocation),
+                MessageCsvType.ManualWebForm => m.Manage<NoTimeMsgCol>(MessageCsvType.ManualWebForm.ToString(), messageLocation, callQueryLocation, customerQueryLocation, reportLocation),
+                MessageCsvType.Leaf => m.Manage<UnifiedDateUnchangedOffset_SeparateGclid_MsgCol>(MessageCsvType.Leaf.ToString(), messageLocation, callQueryLocation, customerQueryLocation, reportLocation),
+                MessageCsvType.LeafRepo => m.Manage<MessageClass>(MessageCsvType.LeafRepo.ToString(), messageLocation, callQueryLocation, customerQueryLocation, reportLocation),
+                _ => m.Manage<UnifiedDateUnchangedOffset_SeparateGclid_MsgCol>(MessageCsvType.Leaf.ToString(), messageLocation, callQueryLocation, customerQueryLocation, reportLocation),
             };
         }
 
@@ -46,14 +81,15 @@ public class MessageVerbHelper
             IMessageAnalysisReportManager manage = service.GetRequiredService<IMessageAnalysisReportManager>();
             return messageType switch
             {
-                MessageCsvType.Pan => manage.ManageMessageAnalysis<SplitDateMountainOffsetMsgCol>(MessageCsvType.Pan.ToString(), messageLocation, callQueryLocation, customerQueryLocation, reportLocation),
-                MessageCsvType.GAdsLeaf => manage.ManageMessageAnalysis<UnifiedDateUnchangedOffset_SeparateGclid_SourceCantBeEmpty_MsgCol>(MessageCsvType.GAdsLeaf.ToString(), messageLocation, callQueryLocation, customerQueryLocation, reportLocation),
-                MessageCsvType.GAdsLeafRepo => manage.ManageMessageAnalysis<MessageClass>(MessageCsvType.GAdsLeafRepo.ToString(), messageLocation, callQueryLocation, customerQueryLocation, reportLocation),
-                MessageCsvType.ManualWebForm => manage.ManageMessageAnalysis<NoTimeMsgCol>(MessageCsvType.ManualWebForm.ToString(), messageLocation, callQueryLocation, customerQueryLocation, reportLocation),
-                MessageCsvType.Leaf => manage.ManageMessageAnalysis<UnifiedDateUnchangedOffset_SeparateGclid_MsgCol>(MessageCsvType.Leaf.ToString(), messageLocation, callQueryLocation, customerQueryLocation, reportLocation),
-                MessageCsvType.LeafRepo => manage.ManageMessageAnalysis<MessageClass>(MessageCsvType.LeafRepo.ToString(), messageLocation, callQueryLocation, customerQueryLocation, reportLocation),
-                _ => manage.ManageMessageAnalysis<UnifiedDateUnchangedOffset_SeparateGclid_MsgCol>(MessageCsvType.Leaf.ToString(), messageLocation, callQueryLocation, customerQueryLocation, reportLocation),
+                MessageCsvType.Pan => manage.Manage<SplitDateMountainOffsetMsgCol>(MessageCsvType.Pan.ToString(), messageLocation, callQueryLocation, customerQueryLocation, reportLocation),
+                MessageCsvType.GAdsLeaf => manage.Manage<UnifiedDateUnchangedOffset_SeparateGclid_SourceCantBeEmpty_MsgCol>(MessageCsvType.GAdsLeaf.ToString(), messageLocation, callQueryLocation, customerQueryLocation, reportLocation),
+                MessageCsvType.GAdsLeafRepo => manage.Manage<MessageClass>(MessageCsvType.GAdsLeafRepo.ToString(), messageLocation, callQueryLocation, customerQueryLocation, reportLocation),
+                MessageCsvType.ManualWebForm => manage.Manage<NoTimeMsgCol>(MessageCsvType.ManualWebForm.ToString(), messageLocation, callQueryLocation, customerQueryLocation, reportLocation),
+                MessageCsvType.Leaf => manage.Manage<UnifiedDateUnchangedOffset_SeparateGclid_MsgCol>(MessageCsvType.Leaf.ToString(), messageLocation, callQueryLocation, customerQueryLocation, reportLocation),
+                MessageCsvType.LeafRepo => manage.Manage<MessageClass>(MessageCsvType.LeafRepo.ToString(), messageLocation, callQueryLocation, customerQueryLocation, reportLocation),
+                _ => manage.Manage<UnifiedDateUnchangedOffset_SeparateGclid_MsgCol>(MessageCsvType.Leaf.ToString(), messageLocation, callQueryLocation, customerQueryLocation, reportLocation),
             };
         }
+        //*/
     }
 }
