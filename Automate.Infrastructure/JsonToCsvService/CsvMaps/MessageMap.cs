3e84ad1f0cdea4d4806375fd05b3478417eab25b
WeCodeAwayYourProblems <90477602+WeCodeAwayYourProblems@users.noreply.github.com>
2024-11-05 11:42:35 -0700

    Bug fixes

--- Automate.Infrastructure/JsonToCsvService/CsvMaps/MessageMap.cs
+++ Automate.Infrastructure/JsonToCsvService/CsvMaps/MessageMap.cs
@@ -8,16 +8,16 @@ public class MessageMap : ClassMap<JsonMessage>
     public MessageMap()
     {
         int index = 0;
-        Map(m=>m.Date).Index(index++).Name("Date");
-        Map(m=>m.FirstName).Index(index++).Name("FirstName");
-        Map(m=>m.LastName).Index(index++).Name("LastName");
-        Map(m=>m.Phone).Index(index++).Name("Phone");
-        Map(m=>m.Email).Index(index++).Name("Email");
-        Map(m=>m.Problem).Index(index++).Name("Problem");
-        Map(m=>m.Branch).Index(index++).Name("Branch");
-        Map(m=>m.ReferringUrl).Index(index++).Name("ReferringUrl");
-        Map(m=>m.FormPageUrl).Index(index++).Name("FormPageUrl");
-        Map(m=>m.CurrentCustomer).Index(index++).Name("CurrentCustomer");
-        Map(m=>m.Zip).Index(index++).Name("Zip");
+        Map(m => m.Date).Index(index++).Name("Date");
+        Map(m => m.FirstName).Index(index++).Name("FirstName");
+        Map(m => m.LastName).Index(index++).Name("LastName");
+        Map(m => m.Phone).Index(index++).Name("Phone");
+        Map(m => m.Email).Index(index++).Name("Email");
+        Map(m => m.Problem).Index(index++).Name("Problem");
+        Map(m => m.Branch).Index(index++).Name("Branch");
+        Map(m => m.ReferringUrl).Index(index++).Name("ReferringUrl");
+        Map(m => m.FormPageUrl).Index(index++).Name("FormPageUrl");
+        Map(m => m.CurrentCustomer).Index(index++).Name("CurrentCustomer");
+        Map(m => m.Zip).Index(index++).Name("Zip");
     }
 }
--- Automate.Infrastructure/MessageLeadsReportService/MessageReportMap.cs
+++ Automate.Infrastructure/MessageLeadsReportService/MessageReportMap.cs
@@ -1,5 +1,4 @@
-﻿using Automate.Application.InfrastructureInterfaces;
-using Automate.Domain.ValueObjects;
+﻿using Automate.Domain.ValueObjects;
 using Automate.Infrastructure.ReportingService;
 using CsvHelper.Configuration;
 using CsvHelper.Configuration.Attributes;
