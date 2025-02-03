8b3f98dbbf219265a5ceacd39e64e7bd2282ea49
WeCodeAwayYourProblems <90477602+WeCodeAwayYourProblems@users.noreply.github.com>
2025-02-03 12:37:50 -0700

    Update ContactsTranslate.cs

--- Automate.Translation/ContactTranslate/ContactsTranslate.cs
+++ Automate.Translation/ContactTranslate/ContactsTranslate.cs
@@ -1,14 +1,19 @@
 ﻿using Automate.Domain.ValueObjects;
+using Automate.Translation.PhoneNumTranslate;
 
 namespace Automate.Translation.ContactTranslate;
 
 public static class ContactsTranslate
 {
-    // Contacts Entity (Infrastructure) to Contacts (Domain Value Object)
-    public static Contacts Translate(this IContactsEntity entity)
+    /// <summary>
+    /// <para>Accepts <paramref name="entity"/> of type <see cref="IContactsEntity"/> and converts it into <see cref="Contact"/></para>
+    /// </summary>
+    /// <param name="entity"></param>
+    /// <returns></returns>
+    public static Contact Translate(this IContactsEntity entity)
     {
-        PhoneNumber number = new(entity.Phone1);
-        PhoneNumber number2 = entity.Phone2 is null || entity.Phone2 == string.Empty ? new(0) : new(entity.Phone2);
+        PhoneNumber number = PhoneNumberTranslate.Translate(entity.Phone1);
+        PhoneNumber number2 = PhoneNumberTranslate.Translate(entity.Phone2);
         return new(number, number2);
     }
 }
