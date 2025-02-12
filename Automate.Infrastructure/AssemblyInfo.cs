430b06de15a17f70ca81130fa071a79814c19b09
WeCodeAwayYourProblems <90477602+WeCodeAwayYourProblems@users.noreply.github.com>
2025-02-12 12:39:09 -0700

    Update AssemblyInfo.cs

--- Automate.Infrastructure/AssemblyInfo.cs
+++ Automate.Infrastructure/AssemblyInfo.cs
@@ -1,5 +1,3 @@
 ﻿using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("Automate.Infrastructure.Test")]
-[assembly: InternalsVisibleTo("Automate.Application.Message.E2E")]
-[assembly: InternalsVisibleTo("Automate.Application.Discrepancy.E2E")]
