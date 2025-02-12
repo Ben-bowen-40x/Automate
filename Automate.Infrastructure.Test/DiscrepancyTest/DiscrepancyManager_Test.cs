b519460293ec69ad903e9f69280ed3c3e3cc3040
WeCodeAwayYourProblems <90477602+WeCodeAwayYourProblems@users.noreply.github.com>
2025-02-12 12:39:15 -0700

    Update DiscrepancyManager_Test.cs

--- Automate.Infrastructure.Test/DiscrepancyTest/DiscrepancyManager_Test.cs
+++ Automate.Infrastructure.Test/DiscrepancyTest/DiscrepancyManager_Test.cs
@@ -1,5 +1,4 @@
 ﻿using Automate.Application.Discrepancy;
-using Automate.Domain.SolutionFunctionality;
 using Automate.Infrastructure.AnalyzeDiscrepancyService;
 using Automate.Infrastructure.ReportingService;
 using Automate.Infrastructure.Test.TestConfigurations;
@@ -20,18 +19,11 @@ public class DiscrepancyManager_Test
     ]
     public void DiscrepancyManager_ExecutesProperly()
     {
-        // Log this instance as a test
-        object sender = new DiscrepancyManager_Test();
-        string memberName = nameof(DiscrepancyManager_Test);
-        string location = GetFullName.GetMemberName(sender, memberName);
-        StringLogger.NewLog(DateTime.Now, sender, memberName, $"Start Test");
-
         // Assemble
         DiscrepancyManager manager = new(new DiscrepancyService(_settings), new ReportServiceSingleton());
 
         // Act
         Result<FileInfo> result = manager.ManageDiscrepancyAnalysis("", "", "");
-        StringLogger.ProduceLog(DateTime.Now, sender, memberName, $"End Test");
 
         // Assert
         Assert.True(result.IsSuccess);
