49f93584e325d67f49ccb99d2b825f58479b3d13
WeCodeAwayYourProblems <90477602+WeCodeAwayYourProblems@users.noreply.github.com>
2025-02-12 16:25:29 -0700

    Update DiscrepancyCallDbEntity.cs

--- Automate.Infrastructure/DataRetrievalFormats/DiscrepancyCallDbEntity.cs
+++ Automate.Infrastructure/DataRetrievalFormats/DiscrepancyCallDbEntity.cs
@@ -1,14 +1,17 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using System.ComponentModel.DataAnnotations.Schema;
 using Automate.Translation.DiscrepancyTranslate;
+using Automate.Application.InfrastructureInterfaces;
+using Automate.Domain.ValueObjects;
+using Automate.Translation.PhoneNumTranslate;
 
 namespace Automate.Infrastructure.AnalyzeDiscrepancyService;
 
 [Keyless]
-public class DiscrepancyCallDbEntity : ICallBoolStringDateTime
+public class DiscrepancyCallDbEntity : ICallBoolStringDateTime, IPhoneNumberCompatible
 {
     [Column("contact_number_clean")]
-    public long Number { get; set; }
+    public long NumberLong { get; set; }
     [Column("called_at")]
     public DateTime? Date { get; set; }
     [Column("sale_billable")]
@@ -17,4 +20,6 @@ public class DiscrepancyCallDbEntity : ICallBoolStringDateTime
     public int? Duration { get; set; }
     [Column("note")]
     public string? Notes { get; set; }
+    private PhoneNumber? _num;
+    public PhoneNumber Number => _num ??= PhoneNumberTranslate.Translate(NumberLong);
 }
