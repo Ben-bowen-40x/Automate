e2f87f5ebd8eaef3680949cccdcf59672ec8024d
WeCodeAwayYourProblems <90477602+WeCodeAwayYourProblems@users.noreply.github.com>
2025-02-21 12:16:11 -0700

    Update IReportService.cs

--- Automate.Application/InfrastructureInterfaces/IReportService.cs
+++ Automate.Application/InfrastructureInterfaces/IReportService.cs
@@ -10,5 +10,4 @@ public interface IReportService
     Result<FileInfo> GenerateDiscrepancyReport(List<DiscrepancyMatch> matches, string reportLocation);
     Result<FileInfo> GenerateMessageLeadReport(string reportDefault, List<QualifiedMessageRecord> messages, string reportLocation);
     Result<FileInfo> GenerateLeafMessages(List<IMessage> msgs, string location);
-    Result<FileInfo> GenerateMessageLeadReportAppend(string reportDefault, List<QualifiedMessageRecord> messages, string reportLocation);
 }
