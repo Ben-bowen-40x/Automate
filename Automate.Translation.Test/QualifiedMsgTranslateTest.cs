cabd054f8f23a9b5096da6f8aaa6042d02ef8e4e
WeCodeAwayYourProblems <90477602+WeCodeAwayYourProblems@users.noreply.github.com>
2025-02-27 12:17:45 -0700

    Update QualifiedMsgTranslateTest.cs

--- Automate.Translation.Test/QualifiedMsgTranslateTest.cs
+++ Automate.Translation.Test/QualifiedMsgTranslateTest.cs
@@ -3,7 +3,6 @@ using Automate.Translation.CustomerTranslate;
 using Automate.Translation.MessageTranslate;
 using Automate.Translation.PhoneNumTranslate;
 using Automate.Translation.QualifiedMessageTranslate;
-using Automate.Translation.Test.ValueObjectsTranslationsTests;
 using NSubstitute;
 
 namespace Automate.Translation.Test;
@@ -16,9 +15,9 @@ public class QualifiedMsgTranslateTest
         // long Number, DateTimeOffset Date, string? Contents, string? Source, bool ImLead, bool SalesLead, long CustomeriD, long subId, bool subIsActive, DateTime CustomerStartDate, DateTime CustomerCancelDate, bool CompletedInitial, double ContractValue, DateTime SubStartDate, DateTime SubCancelDate, string? Sellers
         InlineData
         (
-            //year, month, day, hour, minutes, seconds
+            //year, month, day, hour, minutes, seconds, offset hours
             9876543210,                            //long Number
-            new int[] { 2014, 01, 15, 21, 35, 21 },     //DateTimeOffset Date 
+            new int[] { 2014, 01, 15, 21, 35, 21, -7 }, //DateTimeOffset Date 
             null,                                       //string? Contents
             null,                                       //string? Source 
             false,                                      //bool ImLead
@@ -26,19 +25,19 @@ public class QualifiedMsgTranslateTest
             987654321,                                  //long CustomerId
             654987321,                                  //long SubId
             true,                                       //bool SubIsActive
-            new int[] { 2014, 02, 15, 21, 35, 21 },     //DateTime CustomerStartDate
-            new int[] { 0, 0, 0, 0, 0, 0 },             //DateTime CustomerCancelDate
+            new int[] { 2014, 02, 15, 21, 35, 21, -7 }, //DateTimeOffset CustomerStartDate
+            new int[] { 0, 0, 0, 0, 0, 0, 0 },          //DateTime CustomerCancelDate
             true,                                       //CompletedInitial
             123.01,                                     //ContractValue
-            new int[] { 2014, 02, 15, 21, 35, 21 },     //DateTime SubStartDate
-            new int[] { 0, 0, 0, 0, 0, 0 },             //DateTime SubCancelDate
+            new int[] { 2014, 02, 15, 21, 35, 21, -7 }, //DateTimeOffset SubStartDate
+            new int[] { 0, 0, 0, 0, 0, 0, 0 },          //DateTime SubCancelDate
             null                                        //Sellers
         ),
         InlineData
         (
             //year, month, day, hour, minutes, seconds
             8876543210,                            //long Number
-            new int[] { 2013, 01, 15, 21, 35, 21 },     //DateTimeOffset Date 
+            new int[] { 2013, 01, 15, 21, 35, 21, -7 }, //DateTimeOffset Date 
             null,                                       //string? Contents
             null,                                       //string? Source 
             false,                                      //bool ImLead
@@ -46,47 +45,49 @@ public class QualifiedMsgTranslateTest
             987654320,                                  //long CustomerId
             654987325,                                  //long SubId
             true,                                       //bool SubIsActive
-            new int[] { 2013, 02, 15, 21, 35, 21 },     //DateTime CustomerStartDate
-            new int[] { 0, 0, 0, 0, 0, 0 },             //DateTime CustomerCancelDate
+            new int[] { 2013, 02, 15, 21, 35, 21, -7 }, //DateTimeOffset CustomerStartDate
+            new int[] { 0, 0, 0, 0, 0, 0, 0 },           //DateTime CustomerCancelDate
             true,                                       //CompletedInitial
             4581.2234892,                               //ContractValue
-            new int[] { 2013, 02, 15, 21, 35, 21 },     //DateTime SubStartDate
-            new int[] { 0, 0, 0, 0, 0, 0 },             //DateTime SubCancelDate
+            new int[] { 2013, 02, 15, 21, 35, 21, -7 }, //DateTimeOffset SubStartDate
+            new int[] { 0, 0, 0, 0, 0, 0, 0 },          //DateTime SubCancelDate
             "Ham, Sam, Jam, Alacazam"                   //Sellers
         ),
     ]
     #endregion
     public void IQualifiedMessageTranslate_TranslatesTo_QualifiedMessageRecord(long number, int[] dateInts, string? contents, string? source, bool imLead, bool salesLead, long custId, long subId, bool subIsActive, int[] custDateInts, int[] custCxlDateInts, bool completedInitial, double contractValue, int[] subStartInts, int[] subCxlInts, string? sellers)
     {
-        #region Setup
+        #region Assemble
         // Convert datetimes
-        DateTime date = ConvertDateTimeOffsetTests.MakeDateFromIntArray(dateInts[0], dateInts[1], dateInts[2], dateInts[3], dateInts[4], dateInts[5]);
-        DateTime custDate = ConvertDateTimeOffsetTests.MakeDateFromIntArray(custDateInts[0], custDateInts[1], custDateInts[2], custDateInts[3], custDateInts[4], custDateInts[5]);
-        DateTime custCxlDate = ConvertDateTimeOffsetTests.MakeDateFromIntArray(custCxlDateInts[0], custCxlDateInts[1], custCxlDateInts[2], custCxlDateInts[3], custCxlDateInts[4], custCxlDateInts[5]);
-        DateTime subStartDate = ConvertDateTimeOffsetTests.MakeDateFromIntArray(subStartInts[0], subStartInts[1], subStartInts[2], subStartInts[3], subStartInts[4], subStartInts[5]);
-        DateTime subCxlDate = ConvertDateTimeOffsetTests.MakeDateFromIntArray(subCxlInts[0], subCxlInts[1], subCxlInts[2], subCxlInts[3], subCxlInts[4], subCxlInts[5]);
+        DateTimeOffset date = IntArrToDTO(dateInts[0], dateInts[1], dateInts[2], dateInts[3], dateInts[4], dateInts[5], dateInts[6]);
+        DateTimeOffset custDate = IntArrToDTO(custDateInts[0], custDateInts[1], custDateInts[2], custDateInts[3], custDateInts[4], custDateInts[5], custDateInts[6]);
+        DateTimeOffset custCxlDate = IntArrToDTO(custCxlDateInts[0], custCxlDateInts[1], custCxlDateInts[2], custCxlDateInts[3], custCxlDateInts[4], custCxlDateInts[5], custCxlDateInts[6]);
+        DateTimeOffset subStartDate = IntArrToDTO(subStartInts[0], subStartInts[1], subStartInts[2], subStartInts[3], subStartInts[4], subStartInts[5], subStartInts[6]);
+        DateTimeOffset subCxlDate = IntArrToDTO(subCxlInts[0], subCxlInts[1], subCxlInts[2], subCxlInts[3], subCxlInts[4], subCxlInts[5], subCxlInts[6]);
 
         // Convert PhoneNumber
         PhoneNumber phNumber = PhoneNumberTranslate.Translate(number);
 
         // Set up a mock object using given parameters
-        IQualifiedMessageTranslate qmock = Substitute.For<IQualifiedMessageTranslate>();
-        qmock.Number.Returns(number);
-        qmock.Date.Returns(date);
-        qmock.Contents.Returns(contents);
-        qmock.Source.Returns(source);
-        qmock.ImLead.Returns(imLead);
-        qmock.SalesLead.Returns(salesLead);
-        qmock.CustomerID.Returns(custId);
-        qmock.SubId.Returns(subId);
-        qmock.SubIsActive.Returns(subIsActive);
-        qmock.CustomerStartDate.Returns(custDate);
-        qmock.CustomerCancelDate.Returns(custCxlDate);
-        qmock.CompletedInitial.Returns(completedInitial);
-        qmock.ContractValue.Returns(contractValue);
-        qmock.SubStartDate.Returns(subStartDate);
-        qmock.SubCancelDate.Returns(subCxlDate);
-        qmock.Sellers.Returns(sellers);
+        IQualifiedMessageTranslate toBeTranslated = Substitute.For<IQualifiedMessageTranslate>();
+        toBeTranslated.Number.Returns(number);
+        toBeTranslated.Phone1.Returns(number);
+        toBeTranslated.Phone2.Returns(number);
+        toBeTranslated.Date.Returns(date);
+        toBeTranslated.Contents.Returns(contents);
+        toBeTranslated.Source.Returns(source);
+        toBeTranslated.ImLead.Returns(imLead);
+        toBeTranslated.SalesLead.Returns(salesLead);
+        toBeTranslated.CustomerID.Returns(custId);
+        toBeTranslated.SubId.Returns(subId);
+        toBeTranslated.SubIsActive.Returns(subIsActive);
+        toBeTranslated.CustomerStartDate.Returns(custDate);
+        toBeTranslated.CustomerCancelDate.Returns(custCxlDate);
+        toBeTranslated.CompletedInitial.Returns(completedInitial);
+        toBeTranslated.ContractValue.Returns(contractValue);
+        toBeTranslated.SubStartDate.Returns(subStartDate);
+        toBeTranslated.SubCancelDate.Returns(subCxlDate);
+        toBeTranslated.Sellers.Returns(sellers);
 
         // Set up expected value for IMessage
         IMessage expectedMessage = Substitute.For<IMessage>();
@@ -98,11 +99,11 @@ public class QualifiedMsgTranslateTest
         ICustomerSubscription expectedSubscription = Substitute.For<ICustomerSubscription>();
         expectedSubscription.CustomerId.Returns(custId);
         expectedSubscription.SubscriptionId.Returns(subId);
-        expectedSubscription.SubscriptionStartDate.Returns(new DateTimeOffset(subStartDate, TimeSpan.FromHours(0)));
+        expectedSubscription.SubscriptionStartDate.Returns(subStartDate);
         expectedSubscription.Number.Returns(phNumber);
-        expectedSubscription.Number2.Returns(PhoneNumberTranslate.Default);
-        expectedSubscription.CustomerCancelDate.Returns(new DateTimeOffset(custCxlDate, TimeSpan.FromHours(0)));
-        expectedSubscription.SubscriptionCancelDate.Returns(new DateTimeOffset(subCxlDate, TimeSpan.FromHours(0)));
+        expectedSubscription.Number2.Returns(phNumber);
+        expectedSubscription.CustomerCancelDate.Returns(custCxlDate);
+        expectedSubscription.SubscriptionCancelDate.Returns(subCxlDate);
         expectedSubscription.CustomerActive.Returns(subIsActive);
         expectedSubscription.SubscriptionActive.Returns(subIsActive);
         expectedSubscription.InitialCompleted.Returns(completedInitial);
@@ -110,11 +111,11 @@ public class QualifiedMsgTranslateTest
         expectedSubscription.Sellers.Returns(CustomerSubscriptionTranslate.VerifySeller(sellers));
 
         // Set up expected value for QualifiedMessageRecord
-        var expectedRecord = new QualifiedMessageRecord(expectedMessage, expectedSubscription, imLead, salesLead);
+        QualifiedMessageRecord expectedRecord = new(expectedMessage, expectedSubscription, imLead, salesLead);
         #endregion
 
         // Act
-        QualifiedMessageRecord actualRecord = qmock.Translate();
+        QualifiedMessageRecord actualRecord = toBeTranslated.Translate();
 
         #region Assert
         // Confirm proper execution
@@ -141,5 +142,11 @@ public class QualifiedMsgTranslateTest
         Assert.Equal(expectedRecord.IsSalesLead, actualRecord.IsSalesLead);
         Assert.Equal(expectedRecord.Billable, actualRecord.Billable);
         #endregion
+    }
+    public static DateTimeOffset IntArrToDTO(int year, int month, int day, int hour, int minutes, int seconds, int offset)
+    {
+        return year == 0 || month == 0 || day == 0
+            ? DateTimeOffset.MinValue
+            : new(new DateTime(year, month, day, hour, minutes, seconds), TimeSpan.FromHours(offset));
     }
 }
