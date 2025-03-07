eb4b23563f4bcee41b59235aefd5751c58698364
WeCodeAwayYourProblems <90477602+WeCodeAwayYourProblems@users.noreply.github.com>
2025-03-07 15:50:04 -0700

    Update ID generation

--- Automate.Domain/ValueObjects/QualifiedMessageRecord.cs
+++ Automate.Domain/ValueObjects/QualifiedMessageRecord.cs
@@ -4,9 +4,9 @@ public record QualifiedMessageRecord(IMessage Message, ICustomerSubscription Cus
 {
     private readonly ulong _id = (ulong)
         (
-            Message.Number.Number + 
             Message.Date.Year + Message.Date.Month + Message.Date.DayOfYear + Message.Date.Hour + Message.Date.Minute + Message.Date.Second + Message.Date.Offset.Minutes +
-            Customer.Date.Year + Customer.Date.Month + Customer.Date.DayOfYear + Customer.Date.Hour + Customer.Date.Minute + Customer.Date.Second + Customer.Date.Offset.Minutes
+            Customer.Date.Year + Customer.Date.Month + Customer.Date.DayOfYear + Customer.Date.Hour + Customer.Date.Minute + Customer.Date.Second + Customer.Date.Offset.Minutes +
+            (Message.Number.Number / (Message.Date.Year % 71))
         );
     public ulong Id => _id;
 };
