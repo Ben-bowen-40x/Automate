f1f9342caa8550929e32cd7c4a9769c03b93d6d7
WeCodeAwayYourProblems <90477602+WeCodeAwayYourProblems@users.noreply.github.com>
2025-03-21 10:46:57 -0600

    Bug fixes

--- Automate.Infrastructure/DataRetrievalFormats/SplitDateUTCOffsetMsgCol.cs
+++ Automate.Infrastructure/DataRetrievalFormats/SplitDateUTCOffsetMsgCol.cs
@@ -6,15 +6,15 @@ namespace Automate.Infrastructure.DataRetrievalFormats;
 
 public class SplitDateUTCOffsetMsgCol : IMsgZoneEnumStr
 {
-    [Name("phone_number")]
+    [Name("phone_number", "Phone Number")]
     public string? Number { get; set; }
-    [Name("date_submitted")]
+    [Name("date_submitted", "Date")]
     public string? Date { get; set; }
-    [Name("time_submitted")]
+    [Name("time_submitted", "Time")]
     public string? Time { get; set; }
-    [Name("how_can_we_help")]
+    [Name("how_can_we_help", "Message")]
     public string? Contents { get; set; }
-    [Name("page_name")]
+    [Name("page_name", "Commercial?")]
     public string? Source { get; set; }
 
     public TimeZoneEnum TimeZone => TimeZoneEnum.Utc;
