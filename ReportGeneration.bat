6d101a48c981b8bc3f1f9a925dff949b6d5956f3
WeCodeAwayYourProblems <90477602+WeCodeAwayYourProblems@users.noreply.github.com>
2025-04-02 09:26:04 -0600

    Changes to bat files

--- ReportGeneration.bat
+++ ReportGeneration.bat
@@ -18,14 +18,22 @@ rem Pan Report
 set panReport=%errorlevel%
 "%USERPROFILE%\Repos\Automate\Automate.Cli\bin\Debug\net8.0\Automate.Cli.exe" convertJsonToCsv -t DwhContactForms -j "%USERPROFILE%\Repos\Automate\Automate.Infrastructure\.info\MessageAnalysis\ContactFormsDWH2024json.json" -c "%USERPROFILE%\Repos\Automate\Automate.Infrastructure\.info\MessageAnalysis\DWHforms2024.csv"
 set converted=%errorlevel%
+echo 
+rem Ask the user to perform the final manual task
 if %converted%==0 (
 	echo Please place Manual web forms into the following file: 
 	echo "%USERPROFILE%\Repos\Automate\Automate.Infrastructure\.info\MessageAnalysis\ManualWebForms.csv"
 ) else (
-	echo The conversion process was not successful. Please execute this manually and place manual web forms into the following file: 
+	echo The conversion process was unsuccessful. 
+	echo Please execute the convertJsonToCsv verb as follows ...
+	echo "%USERPROFILE%\Repos\Automate\Automate.Cli\bin\Debug\net8.0\Automate.Cli.exe" convertJsonToCsv -t DwhContactForms -j "%USERPROFILE%\Repos\Automate\Automate.Infrastructure\.info\MessageAnalysis\ContactFormsDWH2024json.json" -c "%USERPROFILE%\Repos\Automate\Automate.Infrastructure\.info\MessageAnalysis\DWHforms2024.csv"
+	echo	(That will place the web forms in the following file:
+	echo	"%USERPROFILE%\Repos\Automate\Automate.Infrastructure\.info\MessageAnalysis\DWHforms2024.csv")
+	echo ... and add the contents of the latter file into the following file manually:
 	echo "%USERPROFILE%\Repos\Automate\Automate.Infrastructure\.info\MessageAnalysis\ManualWebForms.csv" 
 )
 pause
+rem ManualWebForms report
 "%USERPROFILE%\Repos\Automate\Automate.Cli\bin\Debug\net8.0\Automate.Cli.exe" analyzeMessages -c "%USERPROFILE%\Repos\Automate\Automate.Infrastructure\.info\ApiRepos\CallRepo.json" -q "%USERPROFILE%\Repos\Automate\Automate.Infrastructure\.info\ApiRepos\CustomerRepo.json" -as "%USERPROFILE%\Repos\Automate\Automate.Infrastructure\.info\MessageAnalysis\ManualWebForms.csv" -t ManualWebForm -o "%USERPROFILE%\Repos\Automate\Automate.Infrastructure\.info\Reports\ManualWebForms.csv"
 set webFormReport=%errorlevel%
 echo Return codes. Return code 0 indicates success. Any other code indicates failure.
