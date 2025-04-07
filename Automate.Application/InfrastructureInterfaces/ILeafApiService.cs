09f20808da72aff01ab6e732ddc7f0eb0210e835
WeCodeAwayYourProblems <90477602+WeCodeAwayYourProblems@users.noreply.github.com>
2025-04-07 13:21:18 -0600

    renaming

--- Automate.Application/InfrastructureInterfaces/ILeafApiService.cs
+++ Automate.Application/InfrastructureInterfaces/ILeafApiService.cs
@@ -8,6 +8,6 @@ public interface ILeafApiService
     HttpClient GetClient(IHttpClientFactory factory);
     Task<Result<List<TEntity>>> GetLeafThreadsAsync<TEntity>(HttpClient client, int offset = 0, int errorLimit = 5, int sleepInterval = 500, int limit = 1000) where TEntity: class, IConvert;
     Result ReposMatch<TEntity>(out List<IMessage> msgs, out List<TEntity> leaf, string msgRepo = "", string leafRepo = "") where TEntity: class, IConvert;
-    Result Update<TEntity>(List<TEntity> leafRepo, List<TEntity> apiResult, string leafRepoLoc = "") where TEntity: class, IConvert;
-    Result Update<TEntity>(List<TEntity> leafRepo, string leafRepoLoc) where TEntity: class, IConvert;
+    Result Update<TEntity>(List<TEntity> repo, List<TEntity> apiResult, string repoLoc = "") where TEntity: class, IConvert;
+    Result Update<TEntity>(List<TEntity> repo, string repoLoc) where TEntity: class, IConvert;
 }
