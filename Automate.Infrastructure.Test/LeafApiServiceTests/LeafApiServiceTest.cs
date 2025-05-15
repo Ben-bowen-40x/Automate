b5fecbc36c42b73cce97cc2263e2492b6c72ad70
WeCodeAwayYourProblems <90477602+WeCodeAwayYourProblems@users.noreply.github.com>
2025-05-15 16:42:56 -0600

    Set up testing. Tests NOT set up entirely yet

---
+++ Automate.Infrastructure.Test/LeafApiServiceTests/LeafApiServiceTest.cs
@@ -0,0 +1,11 @@
+﻿namespace Automate.Infrastructure.Test.LeafApiServiceTests;
+
+public class LeafApiServiceTest
+{
+    [
+        Theory,
+        InlineData(true),
+        InlineData(false),
+    ]
+    public void EnsureThatIdempotencyIsMaintainedFromOldRepoToNewRepo(bool historicalMatch) { }
+}
