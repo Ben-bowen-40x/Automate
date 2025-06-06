65954a6311ad9d82791265257aca571e4d230ffd
WeCodeAwayYourProblems <90477602+WeCodeAwayYourProblems@users.noreply.github.com>
2025-06-06 09:45:27 -0600

    Add echo: for readability. Add functionality that reads out error logs to the screen, in the event of an error

--- Queries.bat
+++ Queries.bat
@@ -8,6 +8,7 @@ set /p user="Please enter your username: "
 set /p pass="Please enter your password: "
 
 rem Active Not Termite
+echo:
 echo Active not Termite query
 set notTermite="%USERPROFILE%\Repos\Sql-Queries\Current Customers by Service type\Active NOT Termite.sql"
 set notTermiteOutput=%USERPROFILE%\Repos\Automate\Automate.Infrastructure\.info\Reports\QueryReports\NotTermite.tsv
@@ -15,6 +16,7 @@ set notTermiteOutput=%USERPROFILE%\Repos\Automate\Automate.Infrastructure\.info\
 set notTermiteErr=%errorlevel%
 
 rem Active Termite
+echo:
 echo Active Termite Query
 set termite="%USERPROFILE%\Repos\Sql-Queries\Current Customers by Service type\Active Termite only.sql"
 set termiteOutput=%USERPROFILE%\Repos\Automate\Automate.Infrastructure\.info\Reports\QueryReports\Termite.tsv
@@ -22,6 +24,7 @@ set termiteOutput=%USERPROFILE%\Repos\Automate\Automate.Infrastructure\.info\Rep
 set termiteErr=%errorlevel%
 
 rem CornFormation
+echo:
 echo Corn Formation Query
 set cornOutput=%USERPROFILE%\Repos\Automate\Automate.Infrastructure\.info\Reports\QueryReports\CornFormationReport.tsv
 set cornQuery=%USERPROFILE%\Repos\Automate\Automate.Infrastructure\.info\Queries\CornFormation.sql
@@ -29,6 +32,7 @@ set cornQuery=%USERPROFILE%\Repos\Automate\Automate.Infrastructure\.info\Queries
 set corn=%errorlevel%
 
 rem GoonDoggle
+echo:
 echo GoonDoggle Query
 set goonOutput=%USERPROFILE%\Repos\Automate\Automate.Infrastructure\.info\Reports\QueryReports\GoonDoggleReport.tsv
 set goonQuery=%USERPROFILE%\Repos\Automate\Automate.Infrastructure\.info\Queries\GoonDoggle.sql
@@ -36,6 +40,7 @@ set goonQuery=%USERPROFILE%\Repos\Automate\Automate.Infrastructure\.info\Queries
 set goon=%errorlevel%
 
 rem MacBang
+echo:
 echo MacBang Query
 set macBangOutput=%USERPROFILE%\Repos\Automate\Automate.Infrastructure\.info\Reports\QueryReports\MacBangReport.tsv
 set macbangQuery=%USERPROFILE%\Repos\Automate\Automate.Infrastructure\.info\Queries\MacBang.sql
@@ -43,6 +48,7 @@ set macbangQuery=%USERPROFILE%\Repos\Automate\Automate.Infrastructure\.info\Quer
 set macBang=%errorlevel%
 
 rem PanFries
+echo:
 echo Pan Fries Query
 set panOutput=%USERPROFILE%\Repos\Automate\Automate.Infrastructure\.info\Reports\QueryReports\PanFriesReport.tsv
 set panQuery=%USERPROFILE%\Repos\Automate\Automate.Infrastructure\.info\Queries\PanFries.sql
@@ -50,6 +56,7 @@ set panQuery=%USERPROFILE%\Repos\Automate\Automate.Infrastructure\.info\Queries\
 set pan=%errorlevel%
 
 rem Lotus
+echo:
 echo Lotus Query
 set lotusOut=%USERPROFILE%\Repos\Automate\Automate.Infrastructure\.info\Reports\QueryReports\LotusReport.tsv
 set lotusQuery=%USERPROFILE%\Repos\Automate\Automate.Infrastructure\.info\Queries\Lotus.sql
@@ -57,27 +64,42 @@ set lotusQuery=%USERPROFILE%\Repos\Automate\Automate.Infrastructure\.info\Querie
 set lotus=%errorlevel%
 
 rem Error Levels
+echo:
 echo Error levels specified here
 echo Not Termite success: %notTermiteErr%
 echo Not Termite output: %notTermiteOutput%
+if not %notTermiteErr%==0 type %notTermiteOutput% rem error messages are placed in the output file
+echo:
 
 echo Termite success: %termiteErr%
 echo Termite output: %termiteOutput%
+if not %termiteErr%==0 type %termiteOutput% rem error messages are placed in the output file
+echo:
 
 echo Corn success: %corn%
 echo Corn output: %cornOutput%
+if not %corn%==0 type %cornOutput% rem error messages are placed in the output file
+echo:
 
 echo Goon success: %goon%
 echo Goon output: %goonOutput%
+if not %goon%==0 type %goonOutput% rem error messages are placed in the output file
+echo:
 
 echo MacBang success: %macBang%
 echo MacBang output: %macBangOutput%
+if not %macBang%==0 type %macBangOutput% rem error messages are placed in the output file
+echo:
 
 echo Pan success: %pan%
 echo Pan output: %panOutput%
+if not %pan%==0 type %panOutput% rem error messages are placed in the output file
+echo:
 
 echo Lotus success: %lotus%
 echo Lotus output: %lotusOut%
+if not %lotus%==0 type %lotusOut% rem error messages are placed in the output file
+echo:
 
 rem Stop if there is an error
 if not "%notTermiteErr%"=="0" goto :pauseExecution
