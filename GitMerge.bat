21d355d71b654a917dd773bf3113551c719c8ea3
WeCodeAwayYourProblems <90477602+WeCodeAwayYourProblems@users.noreply.github.com>
2025-10-15 14:28:27 -0600

    Add a new file to solution: Git Merge

---
+++ GitMerge.bat
@@ -0,0 +1,33 @@
+@echo off
+title Merge git branches from dev
+
+:: Switch to the correct directory
+cd %USERPROFILE%\Repos\Automate
+
+:: Checkout main
+git checkout main
+
+:: Merge dev into main
+git merge dev -m "Batch File Merge on %date% %time%"
+
+:: Push changes 
+git push
+
+:: Switch to test
+git checkout test
+
+:: merge dev into test
+git merge test -m "Match File Merge on %date% %time%"
+
+:: push changes
+git Push
+
+:: checkout into dev again
+git checkout dev
+
+:: push any changes
+git push
+
+:end
+timeout /t 5
+echo.
