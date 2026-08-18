33bcf19317c9b834793b6b453e0ba3c2264dc218
WeCodeAwayYourProblems <90477602+WeCodeAwayYourProblems@users.noreply.github.com>
2026-08-18 13:58:52 -0600

    Update Queries.bat Add new query

--- Queries.bat
+++ Queries.bat
@@ -67,6 +67,7 @@ call :runQuery "MacBang"           "%AUTO%\Queries\MacBang.sql"                 
 call :runQuery "Pan Fries"         "%AUTO%\Queries\PanFries.sql"                    "%AUTO%\Reports\QueryReports\PanFriesReport.tsv"         dwh_internetmarketingdb || goto :failed
 call :runQuery "Lotus"             "%AUTO%\Queries\Lotus.sql"                       "%AUTO%\Reports\QueryReports\LotusReport.tsv"            dwh_internetmarketingdb || goto :failed
 call :runQuery "Yeller ROI"        "%AUTO%\Queries\YellerROI.sql"                   "%AUTO%\Reports\QueryReports\YellerROI.tsv"              dwh_internetmarketingdb || goto :failed
+call :runQuery "Venn Diagram"      "%AUTO%\Queries\VennDiagram.sql"                 "%AUTO%\Reports\QueryReports\VennDiagram.tsv"            dwh_internetmarketingdb || goto :failed
 ::call :runQuery "Kathartic"         "%AUTO%\Queries\KatharticSummary.sql"            "%AUTO%\Reports\QueryReports\KatharticSummary.tsv"       dwh_ctmdb               || goto :failed
 ::call :runQuery "Upsilon"           "%AUTO%\Queries\Upsilon.sql"                     "%AUTO%\Reports\QueryReports\UpsilonOut.tsv"             dwh_ctmdb               || goto :failed
 ::call :runQuery "Giggle Custard"    "%AUTO%\Queries\GiggleCustardQuery.sql"          "%AUTO%\Reports\QueryReports\GigglyCustard.tsv"          dwh_reportsdb           || goto :failed
